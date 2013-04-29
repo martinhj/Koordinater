56183a0adb89acebf8ace9d29e9eaf842262f313
martinhj <martinhj@gmail.com>
2013-04-29 11:23:00 +0200

    Nå fungerer den. Husk å sjekke Serial.list()

--- arduino/Code_to_recive_serial_data_on_serial_port/Code_to_recive_serial_data_on_serial_port.ino
+++ arduino/Code_to_recive_serial_data_on_serial_port/Code_to_recive_serial_data_on_serial_port.ino
@@ -3,16 +3,18 @@
 // Wiring/Arduino code:
 // Read data from the serial and turn ON or OFF a light depending on the value
 
-Servo servos [2];
 Servo servo1;
-Servo servo2; 
+Servo servo2;
+ 
 int val; // Data received from the serial port
 
 const int servoPin1 = 3;
 const int servoPin2 = 5;
+const int NUMBER_OF_SERVOS = 2;
 
 //int ledPin = 13; // Set the pin to digital I/O 13
 //int ledPins []  = {4, 5, 6, 7};
+int ledPin = 13;
 int drumPins [] = {0,1};
 unsigned long drumLastHit [] = {0, 0, 0, 0};
 boolean drumBusy [] = {false, false, false, false};
@@ -20,10 +22,12 @@ boolean drumBusy [] = {false, false, false, false};
 long time = 0;
  
 void setup() {
+	pinMode(ledPin, OUTPUT);
+	digitalWrite(ledPin, LOW);
 	pinMode(servoPin1, OUTPUT);
 	pinMode(servoPin2, OUTPUT);
-	servos[0].attach(servoPin1);
-	servos[1].attach(servoPin2);
+	servo1.attach(servoPin1);
+	servo2.attach(servoPin2);
 	//servos[0].write(-30);
 	//for (int i = 0; i < sizeof(ledPins); i++) {
 		//pinMode(ledPins[i], OUTPUT);
@@ -31,6 +35,7 @@ void setup() {
 	//}
 	//pinMode(ledPin, OUTPUT); // Set pin as OUTPUT
 	Serial.begin(9600); // Start serial communication at 9600 bps
+
 }
  
 void loop() {
@@ -51,6 +56,7 @@ void loop() {
     hitDrum(val);
     disengageAll();
     val = ' ';
+
  }
 
 /*void blinkLed(int i) {
@@ -62,28 +68,37 @@ void loop() {
 
 
 void hitDrum(int value) {
-	if (val == 0) engage(0);
-	if (val == 1) engage(0);
-	if (val == 2) engage(0);
-	if (val == 3) engage(0);
+	//if (value == 0) engage(0);
+	if (value == 1) engage(0);
+	if (value == 2) engage(0);
+	if (value == 3) engage(0);
 }
 
 void engage(int i) {
+	digitalWrite(ledPin, HIGH);
 	if (drumBusy[i] == false && millis() - drumLastHit[i] > 200) {
 		drumBusy[i] = true;
 		drumLastHit[i] = millis();
-		servos[i].write(0);
+		servoWrite(i, 30);
 	}
 }
 void disengage(int i) {
-	servos[i].write(-30);
+	servoWrite(i, 0);
 }
 
+void servoWrite(int servo, int position) {
+	
+	if (servo == 0) servo1.write(position);
+	if (servo == 1) servo2.write(position);
+}
+
+
 void disengageAll() {
-	for (int i = 0; i < sizeof(servos); i++) {
+	for (int i = 0; i < NUMBER_OF_SERVOS; i++) {
 		if (drumBusy[i] == true && millis() - drumLastHit[i] > 100) {
 			drumBusy[i] = false;
 			disengage(i);
+			digitalWrite(ledPin, LOW);
 		}
 	}
 }
