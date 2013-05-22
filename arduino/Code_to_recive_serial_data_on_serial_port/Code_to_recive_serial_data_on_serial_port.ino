9d5af85e2646a48bc90827ff8eef2e4f85e28ec9
martinhj <martinhj@gmail.com>
2013-05-22 09:59:45 +0200

    Changed the code to have constants for each servo

--- arduino/Code_to_recive_serial_data_on_serial_port/Code_to_recive_serial_data_on_serial_port.ino
+++ arduino/Code_to_recive_serial_data_on_serial_port/Code_to_recive_serial_data_on_serial_port.ino
@@ -7,80 +7,89 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
- 
+
 int val;
 
 const int servoPin1 = 7;
 const int servoPin2 = 8;
-const int servoPin3 = 9;
-const int servoPin4 = 10;
+const int servoPin3 = 9; //ritsjratsj
+const int servoPin4 = 10; 
 const int NUMBER_OF_SERVOS = 4;
-const int TIME_TO_RESET_SERVO = 100;
-const int DELAY_BETWEEN_HITS = 400;
+//const int TIME_TO_RESET_SERVO = 100;
+//const int DELAY_BETWEEN_HITS = 400;
 
 
 int ledPin = 13;
-unsigned long drumLastHit [] = {0, 0, 0, 0};
-boolean drumBusy [] = {false, false, false, false};
+int drumPins [] = {
+  0,1};
+unsigned long drumLastHit [] = {
+  0, 0, 0, 0};
+int delayBetweenHits [] = {400, 400, 800, 400};
+int timeToResetServo [] = {100, 100, 100, 100};
+int servoEngagePosition [] = {30, 30, 30, 30};
+int servoDisengagePosition [] = {0, 0, 0, 0};
+boolean drumBusy [] = {
+  false, false, false, false};
 
 long time = 0;
- 
+
 void setup() {
-	pinMode(ledPin, OUTPUT);
-	digitalWrite(ledPin, LOW);
-	pinMode(servoPin1, OUTPUT);
-	pinMode(servoPin2, OUTPUT);
-	servo1.attach(servoPin1);
-	servo2.attach(servoPin2);
-	servo3.attach(servoPin3);
-	servo4.attach(servoPin4);
-	// 9600 bps må korespondere med hva som brukes i processing.
-	Serial.begin(9600);
-	disengageAll();
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  pinMode(servoPin1, OUTPUT);
+  pinMode(servoPin2, OUTPUT);
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  servo3.attach(servoPin3);
+  servo4.attach(servoPin4);
+  // 9600 bps må korespondere med hva som brukes i processing.
+  Serial.begin(9600);
+  disengageAll();
 }
 void loop() {
-	// leser bare hvis det er data på serial-linja.
- 	if (Serial.available()) { 
-		val = Serial.read();
- 	}
-    hitDrum(val);
-    disengageAll();
-    // reset read value.
-    val = ' ';
- }
+  // leser bare hvis det er data på serial-linja.
+  if (Serial.available()) {
+    val = Serial.read();
+  }
+  hitDrum(val);
+  disengageAll();
+  // reset read value.
+  val = ' ';
+}
 void hitDrum(int value) {
-	if (value == 0) engage(0);
-	if (value == 1) engage(1);
-	// i og med at det kun er to tilkoblede motorer blir kun motor 0 og 1 satt
-	// igang.
-	if (value == 2) engage(2);
-	if (value == 3) engage(3);
+  if (value == 0) engage(0);
+  if (value == 1) engage(1);
+  // i og med at det kun er to tilkoblede motorer blir kun motor 0 og 1 satt
+  // igang.
+  if (value == 2) engage(2);
+  if (value == 3) engage(3);
 }
 void engage(int i) {
-	digitalWrite(ledPin, HIGH);
-	if (drumBusy[i] == false 
-		&& millis() - drumLastHit[i] > DELAY_BETWEEN_HITS) {
-		drumBusy[i] = true;
-		drumLastHit[i] = millis();
-		servoWrite(i, 30);
-	}
+  digitalWrite(ledPin, HIGH);
+  if (drumBusy[i] == false
+    && millis() - drumLastHit[i] > delayBetweenHits[i]) {
+    drumBusy[i] = true;
+    drumLastHit[i] = millis();
+    servoWrite(i, servoEngagePosition[i]);
+  }
 }
 void disengage(int i) {
-	servoWrite(i, 0);
+  servoWrite(i, servoDisengagePosition[i]);
 }
 void servoWrite(int servo, int position) {
-	if (servo == 0) servo1.write(position);
-	if (servo == 1) servo2.write(position);
-	if (servo == 2) servo3.write(position);
-	if (servo == 3) servo4.write(position);
+  if (servo == 0) servo1.write(position);
+  if (servo == 1) servo2.write(position);
+  if (servo == 2) servo3.write(position);
+  if (servo == 3) servo4.write(position);
 }
 void disengageAll() {
-	for (int i = 0; i < NUMBER_OF_SERVOS; i++) {
-		if (drumBusy[i] == true 
-			&& millis() - drumLastHit[i] > TIME_TO_RESET_SERVO) {
-			drumBusy[i] = false;
-			disengage(i);
-			digitalWrite(ledPin, LOW);
-		}
-	}
+  for (int i = 0; i < NUMBER_OF_SERVOS; i++) {
+    if (drumBusy[i] == true
+      && millis() - drumLastHit[i] > timeToResetServo[i]) {
+      drumBusy[i] = false;
+      disengage(i);
+      digitalWrite(ledPin, LOW);
+    }
+  }
 }
+
